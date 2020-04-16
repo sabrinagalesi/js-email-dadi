e26489f19d0c9648be05b474b2f18e12f712b180
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-04-16 11:13:45 +0200

    correzione esercizio

--- Esercizio-email/javascript/text.js
+++ Esercizio-email/javascript/text.js
@@ -14,10 +14,10 @@ var listaEmail = [
     "massimogalesi@gmail.com",
 ];
 
-document.getElementById("spazio").innerHTML= listaEmail.length;
-
 //Variabile che controlla se ho trovato la mia email
 var trovato = false; //Diamo come valore di default che non l'abbiamo trovato
+var counterTotali = 0; //Numero degli elementi totali esaminati 
+var counterNonTrovati = 0; //Numero di email non uguali a quella cercata
 
 //Listiamo il nostro array...
 for(var counter = 0; counter < listaEmail.length; counter++) {
@@ -26,21 +26,27 @@ for(var counter = 0; counter < listaEmail.length; counter++) {
     //Effettuo il controllo se è la mia email inserita!
     if (richiestaEmail == listaEmail[counter]) {
         trovato = true;
-        document.getElementById("spazio-2").innerHTML = listaEmail.length - 1;
     } else {
-        document.getElementById("spazio-2").innerHTML = listaEmail.length;
+        //Se non l'ho trovata incremento il mio counter 
+        counterNonTrovati++;
     }
+
+    counterTotali++;
 }
 
-var coloreAccesso= document.getElementById("stampa-email");
+//Stampa su html 
+var coloreAccesso = document.getElementById("stampa-email");
 
 if (trovato == true) { //oppure if(trovato)
-    document.getElementById("stampa-email").innerHTML= "Compliments! Accesso consentito :)";
+    coloreAccesso.innerHTML = "Compliments! Accesso consentito :)";
     coloreAccesso.className = "verde";
 } else {
-    document.getElementById("stampa-email").innerHTML="Peccato! Accesso nekkato xD"
+    coloreAccesso.innerHTML = "Peccato! Accesso nekkato xD"
     coloreAccesso.className = "rosso";
 }
+
+document.getElementById("spazio").innerHTML= counterTotali;
+document.getElementById("spazio-2").innerHTML = counterNonTrovati;
 
 /*Scrivere il numero degli elementi esaminati nel for che NON erano 
   corrispondenti alla tua email digitata */
